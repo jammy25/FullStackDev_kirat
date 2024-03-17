0989318c394d40341d8a0b51a8d45b4a6fb3eed1
Kishan <kishan95.jammy@gmail.com>
2024-03-17 07:51:05 +0530

    Level-2 js topics

--- 0-1 dev/Week 1/level-02/01-Class.js
+++ 0-1 dev/Week 1/level-02/01-Class.js
@@ -1,1 +1,15 @@
-class Animal{}
+class Animal{
+    constructor(name, legCount, speaks){
+        this.name = name;
+        this.legCount = legCount;
+        this.speaks = speaks;
+    }
+    speak(){
+        console.log(this.name + " makes a " + this.speaks + " noise");   // func inside class
+    }
+}
+
+let dog = new Animal("sheru", 4, "bow wow");   // Obj
+let cat = new Animal("billi", 4, "meow meow");
+
+cat.speak();   // calls func on obj
